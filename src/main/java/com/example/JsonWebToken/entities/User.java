a315df9ec99c08a41bd43d771b7cdac0fb8d388e
axelmaya69 <axeldbmaya96@gmail.com>
2025-03-29 22:34:01 -0600

    agregando getters y setters

--- src/main/java/com/example/JsonWebToken/entities/User.java
+++ src/main/java/com/example/JsonWebToken/entities/User.java
@@ -2,6 +2,8 @@ package com.example.JsonWebToken.entities;
 
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -13,25 +15,35 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(nullable = false)
+    @Getter
+    @Setter
     private Integer id;
 
     @Column(nullable = false)
+    @Getter
+    @Setter
     private String fullName;
 
     @Column(unique = true, length = 100, nullable = false)
+    @Getter
+    @Setter
     private String email;
 
     @Column(nullable = false)
+    @Getter
+    @Setter
     private String password;
 
     @CreationTimestamp
     @Column(updatable = false, name = "created_at")
+    @Getter
+    @Setter
     private Date createdAt;
 
     @UpdateTimestamp
     @Column(name = "updated_at")
+    @Getter
+    @Setter
     private Date updatedAt;
-
-    // Getters and setters
-
+    
 }
