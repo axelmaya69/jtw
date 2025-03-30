46b61897659a4d1d8a98baf5a1e68a54b77a5db6
axelmaya69 <axeldbmaya96@gmail.com>
2025-03-29 22:32:22 -0600

    agregando propiedades a clase

--- src/main/java/com/example/JsonWebToken/entities/User.java
+++ src/main/java/com/example/JsonWebToken/entities/User.java
@@ -1,4 +1,37 @@
 package com.example.JsonWebToken.entities;
 
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@Table(name = "users")
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(unique = true, length = 100, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    // Getters and setters
+
 }
