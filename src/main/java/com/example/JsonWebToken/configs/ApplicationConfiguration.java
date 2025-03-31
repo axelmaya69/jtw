6ab3bc9ef86239f225a3e5ea0ca774f864f189e3
axelmaya69 <axeldbmaya96@gmail.com>
2025-03-30 20:21:21 -0600

    agregando metodos en clase ApplicationConfiguration

--- src/main/java/com/example/JsonWebToken/configs/ApplicationConfiguration.java
+++ src/main/java/com/example/JsonWebToken/configs/ApplicationConfiguration.java
@@ -1,6 +1,7 @@
 package com.example.JsonWebToken.configs;
 
 
+import com.example.JsonWebToken.repositories.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,5 +13,37 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
+@Configuration
 public class ApplicationConfiguration {
+    private final UserRepository userRepository;
+
+    public ApplicationConfiguration(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
 }
