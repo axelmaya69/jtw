09a0620f82c8a3111d2db69b4bb9e06fea6a8ce7
axelmaya69 <axeldbmaya96@gmail.com>
2025-03-30 20:22:39 -0600

    Agregando librerias

--- src/main/java/com/example/JsonWebToken/configs/JwtAuthenticationFilter.java
+++ src/main/java/com/example/JsonWebToken/configs/JwtAuthenticationFilter.java
@@ -1,4 +1,22 @@
 package com.example.JsonWebToken.configs;
 
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import java.io.IOException;
+
+@Component
 public class JwtAuthenticationFilter {
 }
