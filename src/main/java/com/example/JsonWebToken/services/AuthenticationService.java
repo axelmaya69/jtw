52d0286b2bbdd0f77b24a9c6ae0f2da35a45eb2e
axelmaya69 <axeldbmaya96@gmail.com>
2025-04-01 21:12:56 -0600

    Agregando atributos 1

---
+++ src/main/java/com/example/JsonWebToken/services/AuthenticationService.java
@@ -0,0 +1,13 @@
+package com.example.JsonWebToken.services;
+
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+
+
+}
