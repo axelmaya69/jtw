fd4ed315cf4a50d3a0bee72e3e6d881d51c8d060
axelmaya69 <axeldbmaya96@gmail.com>
2025-04-01 21:10:02 -0600

    Agregando atributos 1

--- src/main/java/com/example/JsonWebToken/dtos/RegisterUserDto.java
+++ src/main/java/com/example/JsonWebToken/dtos/RegisterUserDto.java
@@ -1,6 +1,12 @@
 package com.example.JsonWebToken.dtos;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class RegisterUserDto  {
+
+    @Getter
+    @Setter
     private String email;
 
     private String password;
