e69ed85de2cfa829a5a060fbc854d6fa2e88eb61
axelmaya69 <axeldbmaya96@gmail.com>
2025-04-02 19:43:53 -0600

    Agregando librerias

--- src/main/java/com/example/JsonWebToken/controller/AuthenticationController.java
+++ src/main/java/com/example/JsonWebToken/controller/AuthenticationController.java
@@ -1,4 +1,12 @@
 package com.example.JsonWebToken.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/auth")
+@RestController
 public class AuthenticationController {
 }
