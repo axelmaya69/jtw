0aa8c6ad56d8252e6217cde3ec7705d200abbacf
axelmaya69 <axeldbmaya96@gmail.com>
2025-04-02 19:51:58 -0600

    Agregando variables

--- src/main/java/com/example/JsonWebToken/responses/LoginResponse.java
+++ src/main/java/com/example/JsonWebToken/responses/LoginResponse.java
@@ -1,4 +1,19 @@
 package com.example.JsonWebToken.responses;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class LoginResponse {
+    @Getter
+    @Setter
+    private String token;
+
+    @Getter
+    @Setter
+    private long expiresIn;
+
+    public String getToken() {
+        return token;
+    }
+
 }
