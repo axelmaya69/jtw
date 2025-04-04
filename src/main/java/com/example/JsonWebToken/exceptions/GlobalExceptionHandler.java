ca2f066d10a410286e6ee40980c1af426f89fb87
axelmaya69 <axeldbmaya96@gmail.com>
2025-04-03 21:54:37 -0600

    Agregando librerias

--- src/main/java/com/example/JsonWebToken/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/JsonWebToken/exceptions/GlobalExceptionHandler.java
@@ -1,4 +1,16 @@
 package com.example.JsonWebToken.exceptions;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.security.SignatureException;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.AccountStatusException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+
 public class GlobalExceptionHandler {
 }
