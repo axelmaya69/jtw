ba734896465e0a18541abe8cedd2507a248a3e27
axelmaya69 <axeldbmaya96@gmail.com>
2025-04-04 21:36:21 -0600

    Editando mensajes

--- src/main/java/com/example/JsonWebToken/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/JsonWebToken/exceptions/GlobalExceptionHandler.java
@@ -20,7 +20,7 @@ public class GlobalExceptionHandler {
 
         if (exception instanceof BadCredentialsException) {
             errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(401), exception.getMessage());
-            errorDetail.setProperty("description", "The username or password is incorrect");
+            errorDetail.setProperty("description", "El usuario o contraseña es incorrecto");
 
             return errorDetail;
         }
