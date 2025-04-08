8132e131130b3fd1ec762b9a58c27873e1ff7ee2
axelmaya69 <axeldbmaya96@gmail.com>
2025-04-07 21:25:04 -0600

    Añadiendo comentario 6

--- src/main/java/com/example/JsonWebToken/repositories/UserRepository.java
+++ src/main/java/com/example/JsonWebToken/repositories/UserRepository.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+//INterfaz para obtener todos los metodos para hacer un crud
+//dentro de la clase usuario, añadiendo uno personalizado que es
+//el de encontrar un email.
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
     Optional<User> findByEmail(String email);
