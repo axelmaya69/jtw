90a0ed1ab2c42d2f756768a18556f151bb684ba0
axelmaya69 <axeldbmaya96@gmail.com>
2025-04-07 21:26:08 -0600

    Añadiendo comentario 7

--- src/main/java/com/example/JsonWebToken/responses/LoginResponse.java
+++ src/main/java/com/example/JsonWebToken/responses/LoginResponse.java
@@ -3,6 +3,9 @@ package com.example.JsonWebToken.responses;
 import lombok.Getter;
 import lombok.Setter;
 
+//Clase para devolver lo del token mediante inyeccion de dependencias
+//aqui se usa esta clase para indicar que se va a devolver un token
+//y una fecha de expiracion en milisegundos.
 public class LoginResponse {
     @Getter
     @Setter
