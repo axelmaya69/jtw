70229ed24d6d38185ddb13aafff41cd1ab0e3582
axelmaya69 <axeldbmaya96@gmail.com>
2025-04-09 21:38:45 -0600

    Añadiendo comentario

--- src/main/java/com/example/JsonWebToken/JsonWebTokenApplication.java
+++ src/main/java/com/example/JsonWebToken/JsonWebTokenApplication.java
@@ -2,7 +2,7 @@ package com.example.JsonWebToken;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//clase main / principal
 @SpringBootApplication
 public class JsonWebTokenApplication {
 
